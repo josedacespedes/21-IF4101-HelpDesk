28507cd24fdc92c02c4c71d981e46843a1a7d4ec
José Céspedes <63388502+JoseKatoche@users.noreply.github.com>
2021-06-19 19:36:11 -0600

    RK21-25 Starting validates

--- HelpDesk-Angular/src/app/login/login.component.ts
+++ HelpDesk-Angular/src/app/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { LoginService } from '../services/login.service';
+import Swal from 'sweetalert2';
+import { Client } from '../models/client.model';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +11,60 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
   ngOnInit() {
   }
+
+  loginForm: FormGroup;
+  client: Client = new Client();
+
+  constructor(private fb: FormBuilder, private route: ActivatedRoute,
+    private loginService: LoginService, private router: Router) {
+    this.loginForm = this.fb.group({
+      id: 0,
+      email: ['', [Validators.required]],
+      password: new FormControl('', [
+        Validators.required,
+        /*Validators.pattern('^[a-zA-Z0-9]{5,8}$')*/
+      ]),
+    })
+  }
+
+  login() {
+    if (!this.loginForm.valid) {
+      return;
+    }
+
+    Swal.fire({ title: "Correcto", timer: 1000 });
+
+    // this.loginService.login(this.loginForm.value).subscribe((result) => {
+    //   if (result) {
+    //     this.client = result;
+    //     this.modal('/home', 'Bienvenido:'+ this.client.name );
+    //   } else {
+    //     this.modal('/login', 'Credentials incorrect')
+    //   }
+    // });
+  }
+
+  // modal(url: string | '', message: String) {
+  //   let timerInterval: any
+  //   Swal.fire({
+  //     title: message,
+  //     html: '',
+  //     timer: 1000,
+  //     didOpen: () => {
+  //       Swal.showLoading()
+  //       timerInterval = setInterval(() => {
+  //       }, 50)
+  //     },
+  //     willClose: () => {
+  //       clearInterval(timerInterval)
+  //     }
+  //   }).then((result) => {
+  //     if (result.dismiss === Swal.DismissReason.timer) {
+  //       this.router.navigateByUrl(url);
+  //     }
+  //   })
+  // }
 
 }
---
+++ HelpDesk-Angular/src/app/models/client.model.ts
@@ -0,0 +1,13 @@
+/*import { Service } from 'service.model';*/
+
+export class Client{
+    public id: number;
+    public name: string;
+    public firstName:string;
+    public secondName:string;
+    public adress:string | undefined;
+    public phone:number | undefined;
+    public secondContact:number | undefined;
+    public email:string;
+    /*public services: ARRAY SERVICES*/
+}
